dfe9c32f888b4aad995d43c1857156cc21e4fa35
JoViGaCa <86113208+JoViGaCa@users.noreply.github.com>
2022-11-18 13:41:32 -0300

    Update projeto1.c

--- projeto1.c
+++ projeto1.c
@@ -130,44 +130,83 @@ void applyFCFS(Processo * proc){
 }
 
 void applyPrioridadeNPremp(Processo * proc){
-  int tempo_atual = 0;
-  Processo * ordenado = (Processo*) malloc(count*sizeof(Processo));
+  int tempo_atual = 0, tempo_total = 0, aux1 = 0;
+  Processo * executaveis = (Processo*) malloc(count*sizeof(Processo));
+  Processo exec, aux;
+  char ultimo = ' ';
+
+  int contador = 0;
 
   for(int i = 0; i < count; i++){
-    ordenado[i] = proc[i];
+    tempo_total += proc[i].tempo;
   }
 
-  bool troca = true;
-  Processo aux;
 
-  while(troca){
-    troca = false;
-    for(int i = 0; i <= count-2; i++){
-      if(ordenado[i].prioridade > ordenado[i+1].prioridade){
-        aux = ordenado[i];
-        ordenado[i] = ordenado[i+1];
-        ordenado[i+1] = aux;
-        troca = true;
+  while(tempo_atual < tempo_total){
+    // criar vetor com os executáveis, que já chegaram
+    contador = 0;
+    for(int i = 0; i < count; i++){
+      if(proc[i].chegada <= tempo_atual){
+        contador++;
+        executaveis = (Processo*) realloc(executaveis, contador*sizeof(Processo));
+        executaveis[contador-1] = proc[i];
       }
     }
-  }
 
-  for(int i = 0; i < count; i++){
-    printf("[%d]--%c--",tempo_atual, ordenado[i].nome);
-    tempo_atual += ordenado[i].tempo;
-    ordenado[i].termino = tempo_atual;
+    // organizar por Prioridade
+    bool troca = true;
+
+    while(troca){
+      troca = false;
+      for(int i = 0; i <= contador-2; i++){
+        if(executaveis[i].prioridade > executaveis[i+1].prioridade){
+          aux = executaveis[i];
+          executaveis[i] = executaveis[i+1];
+          executaveis[i+1] = aux;
+          troca = true;
+        }
+      }
+    }
+
+
+    //achar o de menor Prioridade
+    for(int i = 0; i < contador; i++){
+      aux1=i;
+      if(executaveis[i].tempo != executaveis[i].exec){
+        break;
+      }
+    }
+    exec = executaveis[aux1];
+    for(int i = 0; i < count; i++){
+      if(proc[i].nome == exec.nome){
+        proc[i].termino = tempo_atual + proc[i].tempo;
+        proc[i].exec+= proc[i].tempo;
+      }
+    }
+
+
+    aux1 = 0;
+    if(ultimo != exec.nome){
+      printf("[%d]--%c--", tempo_atual, exec.nome);
+      ultimo = exec.nome;
+    }
+    tempo_atual+= exec.tempo;
+
+
+
   }
   printf("[%d]\n", tempo_atual);
 
   float tmr = 0;
   for(int i = 0; i < count; i++){
-    tmr += (ordenado[i].termino - ordenado[i].chegada);
+    tmr += (proc[i].termino - proc[i].chegada);
+
   }
   tmr = tmr/count;
 
   float tme = 0;
   for(int i = 0; i < count; i++){
-    tme += (ordenado[i].termino - ordenado[i].chegada - ordenado[i].tempo);
+    tme += (proc[i].termino - proc[i].chegada - proc[i].tempo);
   }
   tme = tme/count;
 
@@ -177,17 +216,17 @@ void applyPrioridadeNPremp(Processo * proc){
   printf("Tempo médio de espera: %.2f\n", tme);
 
 
-  free(ordenado);
+  free(executaveis);
   printf("\n\n");
   return;
 }
 
 void applyPrioridadePremp(Processo * proc){
   int tempo_total = 0, tempo_atual = 0;
-  int aux1 = 0, imprime = 0, diferenca = 0;
+  int aux1 = 0, imprime = 0;
   Processo exec, aux;
   Processo * executaveis = (Processo*) malloc(sizeof(Processo));
-
+  char ultimo = ' ';
 
   //calcula o tempo total
   for(int i = 0; i < count; i++){
@@ -196,12 +235,12 @@ void applyPrioridadePremp(Processo * proc){
 
   for(int i = 0; i < count; i++){
     proc[i].exec = 0;
+    proc[i].espera = 0;
   }
 
   int contador = 0;
 
 
-  printf("[%d]", tempo_atual);
   while(tempo_atual < tempo_total){
     // criar vetor com os executáveis, que já chegaram
     contador = 0;
@@ -228,28 +267,55 @@ void applyPrioridadePremp(Processo * proc){
     }
 
 
-    // executar o de maior Prioridade
-    while(diferenca == 0){
-      diferenca = executaveis[aux1].exec - executaveis[aux1].tempo;
-      aux1++;
+
+    //achar o de menor Prioridade
+    for(int i = 0; i < contador; i++){
+      aux1=i;
+      if(executaveis[i].tempo != executaveis[i].exec){
+        break;
+      }
     }
-    printf("%d --", aux1);
+    //executar
     exec = executaveis[aux1];
-    executaveis[aux1].exec++;
+    for(int i = 0; i < count; i++){
+      if(proc[i].nome == exec.nome){
+        proc[i].exec++;
+      } else {
+        if(proc[i].chegada < tempo_atual && proc[i].exec != proc[i].tempo){
+          proc[i].espera++;
+        }
+
+      }
+    }
 
 
-    tempo_atual++;
-    diferenca = 0;
     aux1 = 0;
-
-    //printf("--%c--[%d]", exec.nome, tempo_atual);
-
-    // aumentar a espera do que não foram
-    //
-
-
-
+    if(ultimo != exec.nome){
+      for(int i = 0; i < count; i++){
+        if(proc[i].nome == ultimo && proc[i].exec == proc[i].tempo){
+          proc[i].termino = tempo_atual;
+        }
+      }
+      printf("[%d]--%c--", tempo_atual, exec.nome);
+      ultimo = exec.nome;
+    }
+    tempo_atual++;
   }
+  printf("[%d]", tempo_atual);
+
+  float tmr = 0, tme = 0;
+  for(int i = 0; i < count; i++){
+    tmr += executaveis[i].termino - executaveis[i].chegada;
+    tme += executaveis[i].espera;
+  }
+
+  tmr = tmr/count;
+  tme = tme/count;
+
+  printf("\nTempo médio de resposta: %.2f\n", tmr);
+  printf("Tempo médio de espera: %.2f\n", tme);
+
+  free(executaveis);
 
   return;
 }
@@ -292,10 +358,15 @@ int main(){
 
   }
 
-  //applyFCFS(processos);
+  printf("Escalonamento por FCFS:\n");
+  applyFCFS(processos);
+  printf("\n\n");
 
-  //applyPrioridadeNPremp(processos);
+  printf("Escalonamento por Prioridade não preemptiva:\n");
+  applyPrioridadeNPremp(processos);
+  printf("\n\n");
 
+  printf("Escalonamento por Prioridade Preemptiva:\n");
   applyPrioridadePremp(processos);
 
 
